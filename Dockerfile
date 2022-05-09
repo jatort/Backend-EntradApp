bcc3caafe44ad9e5e6543db662f77bef3da8f434
Richard <rgmorales1@uc.cl>
2022-05-08 22:41:05 -0400

    feat: first setup proyect

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM node:14 as development
+
+RUN mkdir -p /entradapp
+WORKDIR /entradapp
+COPY package*.json .
+RUN npm install
+COPY . .
+RUN npm run build
+
+FROM node:14 as production
+
+WORKDIR /entradapp
+COPY package*.json .
+RUN npm install --only=production
+COPY --from=development /entradapp/build ./build
+CMD ["node", "build/index.js"]
---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import express, { Request, Response } from "express";
+import mongoose from 'mongoose'
+import * as dotenv from 'dotenv';
+
+
+dotenv.config();
+const app = express();
+
+const PORT = process.env.PORT
+const MONGO_URL = process.env.MONGO_URL
+
+app.get("/", (req: Request, res: Response) => {
+  return res.json({
+    status: "success!",
+  });
+});
+
+
+async function run() {
+  // Connect to MongoDB
+  await mongoose.connect(`${MONGO_URL}`);
+}
+
+run()
+.then(result => app.listen(PORT, () => console.log(`app running on port ${PORT}`)))
+.catch(err => console.log(err))
