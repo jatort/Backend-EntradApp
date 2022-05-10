482ff5bd1e1ca0025d7539635be2600c65353d2c
julio <jnandrade@uc.cl>
2022-05-10 19:17:12 -0400

    feat: Model

---
+++ src/schemas/User.ts
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+import bcrypt from "bcryptjs";
+const { isEmail } = require("validator");
+
+const SALT_WORK_FACTOR = 10;
+
+const UserSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    createIndexes: { unique: true },
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: [isEmail, "invalid email"],
+  },
+  password: { type: String, required: true },
+  role: { type: String, required: true },
+});
+
+// Referenciado de https://stackoverflow.com/questions/14588032/mongoose-password-hashing
+
+UserSchema.pre("save", async function save(next) {
+  if (!this.isModified("password")) return next();
+  try {
+    const salt = await bcrypt.genSalt(SALT_WORK_FACTOR);
+    this.password = await bcrypt.hash(this.password, salt);
+    return next();
+  } catch (err: any) {
+    return next(err);
+  }
+});
+
+UserSchema.methods.validatePassword = async function validatePassword(
+  data: string
+) {
+  return await bcrypt.compare(data, this.password);
+};
+
+export const User = mongoose.model("User", UserSchema); // User is the name of the collection
