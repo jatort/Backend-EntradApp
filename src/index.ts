40ccc01dfd91f819e3faf12ce77a7cb81672c6dc
julio <jnandrade@uc.cl>
2022-05-13 15:23:54 -0400

    Morgan routes log

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,19 @@
 import express, { Request, Response } from "express";
 import mongoose from "mongoose";
 import * as dotenv from "dotenv";
-import routes from "./routes/index"
+import morgan from "morgan";
+import routes from "./routes/index";
 
 dotenv.config();
-export const app = express();
+const app = express();
 
 app.use(express.json()); // for parsing application/json
+app.use(morgan("tiny"));
 
 const PORT = process.env.PORT;
 const MONGO_URL = process.env.MONGO_URL;
 
-app.use("/api/v1/", routes)
+app.use("/api/v1/", routes);
 
 async function run() {
   // Connect to MongoDB
