ff08feeb34931ddf56635445e08b3ff3cdd51198
julio <jnandrade@uc.cl>
2022-05-13 23:43:45 -0400

    Swagger integration example

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,22 +1,29 @@
-import { User } from "../schemas/User";
+import { User, IUser } from "../schemas/User";
 import mongoose from "mongoose";
+import { Get, Post, Tags, Body, Path, Route } from "tsoa";
 
-export async function createUser(data: Object) {
-  /*
-  Crea un usuario a partir de los parámetros recibidos en el json data. Se filtran los errores posibles diferenciando 
-  sus mensajes de error y en caso de exito se retorna el modelo usuario.
-  */
-  const user = new User(data);
-  try {
-    await user.save();
-    return user;
-  } catch (err: any) {
-    if (err.code === 11000) {
-      throw new Error("User already exists");
-    } else if (err == mongoose.Error.ValidationError) {
-      throw new Error("Invalid user data");
-    } else if (err.errors.email.message == "invalid email") {
-      throw new Error("Invalid email");
+@Route("user")
+@Tags("user")
+export default class UserController {
+  @Post("/")
+  public async createUser(@Body() body: IUser): Promise<IUser> {
+    /*
+    Crea un usuario a partir de los parámetros recibidos en el json data. Se filtran los errores posibles diferenciando 
+    sus mensajes de error y en caso de exito se retorna el modelo usuario.
+    */
+    try {
+      const user = new User(body);
+      await user.save();
+      return user;
+    } catch (err: any) {
+      if (err.code === 11000) {
+        throw new Error("User already exists");
+      } else if (err == mongoose.Error.ValidationError) {
+        throw new Error("Invalid user data");
+      } else if (err.errors.email.message == "invalid email") {
+        throw new Error("Invalid email");
+      }
+      throw err;
     }
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,27 @@ import mongoose from "mongoose";
 import * as dotenv from "dotenv";
 import morgan from "morgan";
 import routes from "./routes/index";
+import swaggerUi from "swagger-ui-express";
 
 dotenv.config();
 const app = express();
 
 app.use(express.json()); // for parsing application/json
-app.use(morgan("tiny"));
+app.use(morgan("tiny")); // routes log
+app.use(express.static("public")); // static files
 
 const PORT = process.env.PORT;
 const MONGO_URL = process.env.MONGO_URL;
+
+app.use(
+  "/docs",
+  swaggerUi.serve,
+  swaggerUi.setup(undefined, {
+    swaggerOptions: {
+      url: "/swagger.json",
+    },
+  })
+);
 
 app.use("/api/v1/", routes);
 
