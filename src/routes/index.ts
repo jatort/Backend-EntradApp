f64c81ca029c9d058f1263632e32db8b53a80b00
Richard <rgmorales1@uc.cl>
2022-05-17 11:04:22 -0400

    Resolve tests and workflow

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import Router from "express";
 const userRoutes = require("./user");
 const loginRoutes = require("./login");
+const eventRouter = require("./event");
 
 const routes = Router();
 routes.use("/user", userRoutes);
 routes.use("/login", loginRoutes);
+routes.use("/event", eventRouter);
 
 export default routes;
