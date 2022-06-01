561b00fcd37d4dd0f10e0ff544f645a93b7a8591
Richard <rgmorales1@uc.cl>
2022-06-01 10:20:39 -0400

    Add more tests and test pull requests CI

--- src/test/endpoints/event.endpoints.test.ts
+++ src/test/endpoints/event.endpoints.test.ts
@@ -37,6 +37,20 @@ const eventData = {
 
 };
 
+const invalidEventData = {
+  name: "Lollapalooza",
+  category: "Music",
+  date: new Date("2022-06-15"),
+  dateLimitBuy: new Date("2022-06-09"),
+  description:
+    "Lollapalooza es un festival musical de los Estados Unidos que originalmente ofrecía bandas de rock alternativo, indie y punk rock; también hay actuaciones cómicas y de danza.",
+  nTickets: 1000,
+  imageUrl:
+    "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.adnradio.cl%2Fconciertos%2F2021%2F11%2F17%2Flollapalooza-chile-2022-que-lugares-podrian-sustituir-a-parque-ohiggins.html&psig=AOvVaw39bRWA_GrXo6ZWiJ9AOqnM&ust=1652595291018000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCMi5pq2r3vcCFQAAAAAdAAAAABAD",
+  price: 100,
+  city: "Santiago"
+};
+
 const getToken = async (userData: Object) => {
   await request(app).post("/api/v1/user").send(userData);
   let resUser = await request(app).post("/api/v1/login").send(userData);
@@ -75,6 +89,14 @@ describe("Event POST endpoints", () => {
       .set("Authorization", `Bearer ${token}`)
       .send(eventData);
     expect(res.statusCode).toEqual(201);
+  });
+  it("Testing event post with invalid data", async () => {
+    let token = await getToken(userDataProd);
+    const res = await request(app)
+      .post("/api/v1/event")
+      .set("Authorization", `Bearer ${token}`)
+      .send(invalidEventData);
+    expect(res.statusCode).toEqual(400);
   });
 });
 
@@ -118,6 +140,13 @@ describe("Event GET myevents", () => {
       .set("Authorization", `Bearer ${token}`);
     expect(res.statusCode).toBe(400);
     expect(res.body.message).toBe("No events found");
+  });
+  it("Testing event get myevents endpoint with invalid token", async () => {
+    let token = "thisisainvalidtoken"
+    const res = await request(app)
+      .get("/api/v1/user/myevents")
+      .set("Authorization", `Bearer ${token}`);
+    expect(res.statusCode).toBe(400);
   });
 });
 
--- src/test/endpoints/user.endpoints.test.ts
+++ src/test/endpoints/user.endpoints.test.ts
@@ -11,6 +11,34 @@ const userData = {
   password: "TekLoon123",
 };
 
+const userInvalidData = {
+  username: "TekLoon",
+  email: "tekloongmailcom",
+  role: "common",
+  password: "TekLooninvalid",
+};
+
+const userInvalidEmail = {
+  username: "TekLoon",
+  email: "tekloongmailcom",
+  role: "common",
+  password: "TekLooninvalid",
+};
+
+const userInvalidPassword = {
+  username: "TekLoon",
+  email: "tekloon@gmail.com",
+  role: "common",
+  password: "TekLooninvalid",
+};
+
+const invalidUserNoCreated = {
+  username: "TekLoonNotCreated",
+  email: "tekloonnotcreated@gmail.com",
+  role: "common",
+  password: "TekLoon123",
+};
+
 beforeAll(async () => {
   await User.deleteMany({});
 });
@@ -25,5 +53,30 @@ describe("User endpoints", () => {
   it("Testing user post", async () => {
     const res = await request(app).post("/api/v1/user").send(userData);
     expect(res.statusCode).toEqual(201);
+  });
+  it("Testing user post invalid email", async () => {
+    const res = await request(app).post("/api/v1/user").send(userInvalidEmail);
+    expect(res.statusCode).toEqual(400);
+  });
+  it("Testing user post invalid email", async () => {
+    const res = await request(app).post("/api/v1/user").send(userInvalidData);
+    expect(res.statusCode).toEqual(400);
+  });
+});
+
+describe("Login endpoints", () => {
+  it("Testing user login", async () => {
+    const res = await request(app).post("/api/v1/login").send(userData);
+    expect(res.statusCode).toEqual(200);
+  });
+  it("Testing login with user not created", async () => {
+    const res = await request(app).post("/api/v1/login").send(invalidUserNoCreated);
+    expect(res.body.error).toBe('User not found.')
+    expect(res.statusCode).toEqual(400);
+  });
+  it("Testing login with user invalid password", async () => {
+    const res = await request(app).post("/api/v1/login").send(userInvalidPassword);
+    expect(res.body.error).toBe('Invalid password.')
+    expect(res.statusCode).toEqual(400);
   });
 });
