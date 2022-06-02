8fb41590040b949e3a00badba7d15b933664ccd5
Richard <rgmorales1@uc.cl>
2022-06-02 16:49:07 -0400

    (feat): Add route user/id and change user/myevents to user/:id/event

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,6 +1,7 @@
 import { Router, Request, Response } from "express";
 import UserController from "../controllers/userController";
 import { AuthRequest } from "../types/authRequest";
+import { UserResponse } from "../types/userResponse";
 import { auth, isProd } from "../middlewares/auth";
 
 const userRouter = Router();
@@ -15,14 +16,28 @@ userRouter.post("/", async (req: Request, res: Response) => {
   }
 });
 
+userRouter.get("/:id", async (req: Request< {id: string} >, res: Response) => {
+  /*
+  Endpoint para obtener el usuario de id 'id', en caso de exito retorna los datos del evento, en caso contrario se retorna el código 400
+  con el mensaje específico de la causa del error.
+  */
+  const controller = new UserController();
+  try {
+    const user = await controller.getUser(req.params.id);
+    return res.status(200).json({ user });
+  } catch (err: any) {
+    return res.status(400).json({ message: err.message });
+  }
+});
+
 userRouter.get(
-  "/myevents",
+  "/:id/event",
   auth,
   isProd,
-  async (req: AuthRequest, res: Response) => {
+  async (req: Request< {id: string} >, res: Response) => {
     const controller = new UserController();
     try {
-      const events = await controller.getMyEvents(req.user?.email);
+      const events = await controller.getMyEvents(req.params.id);
       return res.status(200).json({ events });
     } catch (err: any) {
       return res.status(400).json({ message: err.message });
--- src/test/endpoints/user.endpoints.test.ts
+++ src/test/endpoints/user.endpoints.test.ts
@@ -11,6 +11,13 @@ const userData = {
   password: "TekLoon123",
 };
 
+const prodData = {
+  username: "TekLoon",
+  email: "tekloon@gmail.com",
+  role: "prod",
+  password: "TekLoon123",
+};
+
 const userInvalidData = {
   username: "TekLoon",
   email: "tekloongmailcom",
@@ -39,6 +46,22 @@ const invalidUserNoCreated = {
   password: "TekLoon123",
 };
 
+// Evento generico con usuario cliente para testear los endpoints
+const eventData = {
+  name: "Lollapalooza",
+  category: "Music",
+  date: new Date("2022-06-15"),
+  dateLimitBuy: new Date("2022-06-09"),
+  description:
+    "Lollapalooza es un festival musical de los Estados Unidos que originalmente ofrecía bandas de rock alternativo, indie y punk rock; también hay actuaciones cómicas y de danza.",
+  nTickets: 1000,
+  imageUrl:
+    "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.adnradio.cl%2Fconciertos%2F2021%2F11%2F17%2Flollapalooza-chile-2022-que-lugares-podrian-sustituir-a-parque-ohiggins.html&psig=AOvVaw39bRWA_GrXo6ZWiJ9AOqnM&ust=1652595291018000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCMi5pq2r3vcCFQAAAAAdAAAAABAD",
+  price: 100,
+  address: "Puchuncaví 3244",
+  city: "Santiago"
+};
+
 beforeAll(async () => {
   await User.deleteMany({});
 });
@@ -47,6 +70,10 @@ afterAll(async () => {
   await mongoose.connection.db.dropDatabase();
   await mongoose.disconnect();
   await stopDb(); 
+});
+
+afterEach(async () => {
+  await User.deleteMany({});
 });
 
 describe("User endpoints", () => {
@@ -62,10 +89,21 @@ describe("User endpoints", () => {
     const res = await request(app).post("/api/v1/user").send(userInvalidData);
     expect(res.statusCode).toEqual(400);
   });
+  it("Testing user get", async () => {
+    const resRegister = await request(app).post("/api/v1/user").send(prodData);
+    const res = await request(app).get(`/api/v1/user/${resRegister.body._id}`);
+    expect(res.statusCode).toEqual(200);
+  });
+  it("Testing user get not created", async () => {
+    const resRegister = await request(app).post("/api/v1/user").send(prodData);
+    const res = await request(app).post(`/api/v1/user/${resRegister.body._id}INVALID`);
+    expect(res.statusCode).toEqual(404);
+  });
 });
 
 describe("Login endpoints", () => {
   it("Testing user login", async () => {
+    await request(app).post("/api/v1/user").send(userData);
     const res = await request(app).post("/api/v1/login").send(userData);
     expect(res.statusCode).toEqual(200);
   });
@@ -75,8 +113,44 @@ describe("Login endpoints", () => {
     expect(res.statusCode).toEqual(400);
   });
   it("Testing login with user invalid password", async () => {
+    await request(app).post("/api/v1/user").send(userData);
     const res = await request(app).post("/api/v1/login").send(userInvalidPassword);
     expect(res.body.error).toBe('Invalid password.')
     expect(res.statusCode).toEqual(400);
+  });
+});
+
+describe("Event GET user/:id/event", () => {
+  it("Testing get user id events endpoint with event created", async () => {
+    const resRegister = await request(app).post("/api/v1/user").send(prodData);
+    let resUser = await request(app).post("/api/v1/login").send(prodData);
+    let token = resUser.body["token"];
+    await request(app)
+      .post("/api/v1/event")
+      .set("Authorization", `Bearer ${token}`)
+      .send(eventData);
+    const res = await request(app)
+      .get(`/api/v1/user/${resRegister.body._id}/event`)
+      .set("Authorization", `Bearer ${token}`);
+    expect(res.body.events.length).toBe(1);
+  });
+  it("Testing get user id events endpoint with no event created", async () => {
+    const resRegister = await request(app).post("/api/v1/user").send(prodData);
+    let resUser = await request(app).post("/api/v1/login").send(prodData);
+    let token = resUser.body["token"];
+
+    const res = await request(app)
+      .get(`/api/v1/user/${resRegister.body._id}/event`)
+      .set("Authorization", `Bearer ${token}`);
+    expect(res.statusCode).toBe(400);
+    expect(res.body.message).toBe("No events found");
+  });
+  it("Testing get user id events endpoint with invalid token", async () => {
+    const resRegister = await request(app).post("/api/v1/user").send(prodData);
+    let token = "thisisainvalidtoken"
+    const res = await request(app)
+      .get(`/api/v1/user/${resRegister.body._id}/event`)
+      .set("Authorization", `Bearer ${token}`);
+    expect(res.statusCode).toBe(400);
   });
 });
